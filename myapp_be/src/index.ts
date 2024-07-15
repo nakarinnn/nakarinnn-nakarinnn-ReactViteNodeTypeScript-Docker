8dc25c7053b4afa3d6d320c09f88fe1db446d91d
nakarinnn <namekung951.15999@gmail.com>
2024-07-15 23:38:02 +0700

    first commit

---
+++ myapp_be/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+import sequelize from './config/database';
+import userRoutes from './routes/userRoutes';
+require('dotenv').config()
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use('/users', userRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Express.js')
+});
+
+app.listen(port, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+    await sequelize.sync();
+    console.log('Database synchronized.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ myapp_be/src/models/user.ts
@@ -0,0 +1,23 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database';
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default User;
